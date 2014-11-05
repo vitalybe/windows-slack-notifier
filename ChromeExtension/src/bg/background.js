3f5c529363e5a69d2a45c0b55c55e0b90d336de1
vitaly - git <vitaly@jajah.com>
2014-11-05 18:36:12 +0200

    Chrome extension - Persistence, reconnection and status communication via badge colors

--- ChromeExtension/src/bg/background.js
+++ ChromeExtension/src/bg/background.js
@@ -10,6 +10,22 @@
 
     var lastAlertLevel = null;
     var lastSender = null;
+    var websocket;
+
+    function hexToRgb(hex) {
+        // Expand shorthand form (e.g. "03F") to full form (e.g. "0033FF")
+        var shorthandRegex = /^#?([a-f\d])([a-f\d])([a-f\d])$/i;
+        hex = hex.replace(shorthandRegex, function(m, r, g, b) {
+            return r + r + g + g + b + b;
+        });
+
+        var result = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(hex);
+        return result ? {
+            r: parseInt(result[1], 16),
+            g: parseInt(result[2], 16),
+            b: parseInt(result[3], 16)
+        } : null;
+    }
 
     function focusOnLastSender() {
         if (lastSender) {
@@ -19,11 +35,30 @@
         }
     }
 
-    var websocket;
     function connect() {
+        setBadge("Disconnected", "000000");
+
         websocket = new WebSocket("ws://localhost:4649/Slack");
-        websocket.onopen = function(evt) { alert("open"); };
-        websocket.onclose = function(evt) { alert("close") };
+        websocket.onopen = function (evt) {
+            setBadge("Connected");
+        };
+        websocket.onclose = function (evt) {
+            //try to reconnect in 5 seconds
+            setTimeout(function () {connect() }, 5000);
+        };
+    }
+
+    // color - Color hex value - If not given, the badge is removed
+    function setBadge(tooltip, color) {
+        if(color) {
+            var rgb = hexToRgb(color);
+            chrome.browserAction.setBadgeBackgroundColor({color: [rgb.r, rgb.g, rgb.b, 255]});
+            chrome.browserAction.setBadgeText({text: " "});
+        } else {
+            chrome.browserAction.setBadgeText({text: ""});
+        }
+
+        chrome.browserAction.setTitle({title: tooltip});
     }
 
     chrome.browserAction.onClicked.addListener(function () {
@@ -36,7 +71,7 @@
 
     chrome.extension.onMessage.addListener(
         function (request, sender, sendResponse) {
-            if (lastAlertLevel === request.alertLevel) {
+            if (websocket.readyState != 1 || lastAlertLevel === request.alertLevel) {
                 return;
             }
 
@@ -44,32 +79,24 @@
             lastSender = sender;
 
             if (request.alertLevel === 2) {
-                chrome.browserAction.setBadgeBackgroundColor({color: [255, 0, 0, 255]});
-                chrome.notifications.create(notificationId, notificationOptions, function () {
-                });
+                setBadge("Important unread", "ff0000");
+            } else if (request.alertLevel === 1) {
+                setBadge("Unread", "ffffff");
             } else {
-                chrome.notifications.clear(notificationId, function () {
-                });
-            }
-
-            if (request.alertLevel === 1) {
-                chrome.browserAction.setBadgeBackgroundColor({color: [255, 255, 255, 255]});
-            }
-
-            if (request.alertLevel > 0) {
-                chrome.browserAction.setBadgeText({text: " "});
-            } else {
-                chrome.browserAction.setBadgeText({text: ""});
+                setBadge("All read");
             }
 
             websocket.send(request.alertLevel);
         });
 
+    // This will try to connect to a server and reconnect if needed
     connect();
-    chrome.windows.getAll({populate:true},function(windows){
-        windows.forEach(function(window){
-            window.tabs.forEach(function(tab){
-                if(tab.url.indexOf("slack.com") >= 0) {
+
+    // Reload all Slack tabs to make sure they get injected with the extension code
+    chrome.windows.getAll({populate: true}, function (windows) {
+        windows.forEach(function (window) {
+            window.tabs.forEach(function (tab) {
+                if (tab.url.indexOf("slack.com") >= 0) {
                     chrome.tabs.reload(tab.id);
                 }
             });
