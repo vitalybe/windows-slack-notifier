c0013ebf917291ced531f129915bd18f286ac4d6
vitaly - git <vitaly@jajah.com>
2015-03-20 16:34:54 +0200

    Refactoring - Splitting SlackRtm

---
+++ SlackWindowsTray/SlackRTM/RtmMessageService.cs
@@ -0,0 +1,107 @@
+using System;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
+using EasyHttp.Http;
+using Newtonsoft.Json;
+using ToastNotifications;
+
+namespace SlackWindowsTray
+{
+    // Parses recieved messages
+    class RtmMessageService
+    {
+        // Channels, groups, users - ID and name
+        private Dictionary<string, string> _slackObjects = new Dictionary<string, string>();
+
+        private string SlackIdToName(string id)
+        {
+            if (!_slackObjects.ContainsKey(id))
+            {
+                RefreshAll();
+            }
+
+            var name = "UNKNOWN";
+            if (_slackObjects.ContainsKey(id))
+            {
+                name = _slackObjects[id];
+            }
+
+            return name;
+        }
+
+        private bool isRefreshing = false;
+
+        private void RefreshAll()
+        {
+            if (isRefreshing)
+            {
+                // Prevent recursion
+                return;
+            }
+
+            isRefreshing = true;
+            try
+            {
+                _slackObjects.Clear();
+                _slackObjects.Add("USLACKBOT", "slackbot");
+
+                RefreshData("channels.list", "channels", "#");
+                RefreshData("users.list", "members");
+                RefreshData("groups.list", "groups");
+                RefreshImData();
+            }
+            finally
+            {
+                isRefreshing = false;
+            }
+        }
+
+        private void RefreshData(string api, string collectionName, string prefix = "")
+        {
+            var dataCollection = Utils.Instance.SlackApiCall(api)[collectionName];
+            foreach (dynamic data in dataCollection)
+            {
+                _slackObjects.Add(data.id.Value, prefix + data.name.Value);
+            }
+        }
+
+        private void RefreshImData()
+        {
+            var dataCollection = Utils.Instance.SlackApiCall("im.list")["ims"];
+            foreach (dynamic data in dataCollection)
+            {
+                _slackObjects.Add(data.id.Value, SlackIdToName(data.user.Value));
+            }
+        }
+
+
+        public void OnMessage(dynamic message)
+        {
+            var channelName = SlackIdToName(message.channel.Value);
+            var user = SlackIdToName(message.user.Value);
+
+            Regex messageIdRegex = new Regex("<@([A-Z0-9]+)>");
+            string text = message.text.Value;
+            text = messageIdRegex.Replace(text, match =>
+            {
+                var id = match.Groups[1].Value;
+                return SlackIdToName(id);
+            });
+
+            MainWindow.Form.UIThread(delegate()
+            {
+                var toastNotification = new Notification(channelName, string.Format("{0}: {1}", user, text),
+                    -1, FormAnimator.AnimationMethod.Slide, FormAnimator.AnimationDirection.Up);
+                toastNotification.Show();
+            });
+
+            Console.WriteLine("[{0}] {1}: {2}", channelName, user, text);
+        }
+
+        public static readonly RtmMessageService Instance = new RtmMessageService();
+        private RtmMessageService()
+        {
+        }
+
+    }
+}
--- SlackWindowsTray/SlackRTM/SlackRtm.cs
+++ SlackWindowsTray/SlackRTM/SlackRtm.cs
@@ -11,11 +11,10 @@ using WebSocketSharp;
 
 namespace SlackWindowsTray
 {
+    // Responsible for connecting to Slack RTM, maintaining the connection and creating events for appropriate listeners
     class SlackRtm
     {
         private WebSocket _webSocket = null;
-        // Channels, groups, users - ID and name
-        private Dictionary<string, string> _slackObjects = new Dictionary<string, string>();
 
         public static readonly SlackRtm Instance = new SlackRtm();
         private SlackRtm()
@@ -29,17 +28,8 @@ namespace SlackWindowsTray
 
         private void ConnectRtm()
         {
-            var rtmInfo = SlackApiCall("rtm.start");
+            var rtmInfo = Utils.Instance.SlackApiCall("rtm.start");
             ConnectWebSocket(rtmInfo.url.Value);
-        }
-
-        private static dynamic SlackApiCall(string apiName)
-        {
-            var url = string.Format("https://slack.com/api/{0}?token={1}", apiName, SlackWindowsTray.Default.SlackToken);
-
-            var http = new HttpClient();
-            var response = http.Get(url);
-            return JsonConvert.DeserializeObject(response.RawText);
         }
 
         private void ConnectWebSocket(string url)
@@ -55,89 +45,8 @@ namespace SlackWindowsTray
             dynamic message = JsonConvert.DeserializeObject(e.Data);
             if (message.type == "message")
             {
-                var channelName = SlackIdToName(message.channel.Value);
-                var user = SlackIdToName(message.user.Value);
-                
-                Regex messageIdRegex = new Regex("<@([A-Z0-9]+)>");
-                string text = message.text.Value;
-                text = messageIdRegex.Replace(text, match =>
-                {
-                    var id = match.Groups[1].Value;
-                    return SlackIdToName(id);
-                });
-
-                MainWindow.Form.UIThread(delegate()
-                {
-                    var toastNotification = new Notification(channelName, string.Format("{0}: {1}", user, text),
-                        -1, FormAnimator.AnimationMethod.Slide, FormAnimator.AnimationDirection.Up);
-                    toastNotification.Show();
-                });
-
-                Console.WriteLine("[{0}] {1}: {2}", channelName, user, text);
+                RtmMessageService.Instance.OnMessage(message);
             }
         }
-
-        private string SlackIdToName(string id)
-        {
-            if (!_slackObjects.ContainsKey(id))
-            {
-                RefreshAll();
-            }
-
-            var name = "UNKNOWN";
-            if (_slackObjects.ContainsKey(id))
-            {
-                name = _slackObjects[id];
-            }
-
-            return name;
-        }
-
-        private bool isRefreshing = false;
-        private Form _owner;
-
-        private void RefreshAll()
-        {
-            if (isRefreshing)
-            {
-                // Prevent recursion
-                return;
-            }
-
-            isRefreshing = true;
-            try
-            {
-                _slackObjects.Clear();
-                _slackObjects.Add("USLACKBOT", "slackbot");
-
-                RefreshData("channels.list", "channels", "#");
-                RefreshData("users.list", "members");
-                RefreshData("groups.list", "groups");
-                RefreshImData();
-            }
-            finally
-            {
-                isRefreshing = false;
-            }
-        }
-
-        private void RefreshData(string api, string collectionName, string prefix = "")
-        {
-            var dataCollection = SlackApiCall(api)[collectionName];
-            foreach (dynamic data in dataCollection)
-            {
-                _slackObjects.Add(data.id.Value, prefix + data.name.Value);
-            }
-        }
-
-        private void RefreshImData()
-        {
-            var dataCollection = SlackApiCall("im.list")["ims"];
-            foreach (dynamic data in dataCollection)
-            {
-                _slackObjects.Add(data.id.Value, SlackIdToName(data.user.Value));
-            }
-        }
-
     }
 }
